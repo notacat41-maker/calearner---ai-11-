ad8b762069550335b3e9ca18fcc58850ae1661db
notacat41-maker <notacat41@gmail.com>
2025-12-07 10:40:16 -0800

    Update vite.config.ts
    8

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
-import path from 'path';
 
 // Declare process to avoid TS2580 error in this config file
 declare const process: any;
@@ -35,14 +34,6 @@ export default defineConfig(({ mode }) => {
         }
       })
     ],
-    resolve: {
-      alias: {
-        // Force resolution of these folders to the project root
-        '@': path.resolve(__dirname, './'),
-        'components': path.resolve(__dirname, './components'),
-        'services': path.resolve(__dirname, './services')
-      }
-    },
     define: {
       'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY || env.API_KEY)
     }
